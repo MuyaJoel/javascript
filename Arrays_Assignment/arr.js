735d6f9f9ee1a66a0f64c5d295caa704b4314ee1
Joel <joelmuya@outlook.com>
2024-05-18 07:43:20 +0300

    new

--- Arrays_Assignment/arr.js
+++ Arrays_Assignment/arr.js
@@ -47,5 +47,80 @@ let arr2= [5,6,7,8,9]
 let newArr = arr1.concat(arr2)//to add arr2 in arr1
 console.log(newArr)
 
+//Flatten Arrays In java script
+//Qn1.Write a function to flatten a nested array in java script
+let nestedArray =[1,[2,3],[4,5,6],7]
+//to flatten the above array we can use methods like; flat(), reduce() and concat(),
+//and using recursion with reduce() and concat()methods
+function flattenArray(arr){
+    let flatArray=arr.reduce((accumulator,value) => accumulator.concat(value), [])
+    console.log(flatArray)
+}
+flattenArray(nestedArray)
+//Qn2.What is the function of the reduce() method in the above function
 
-//still sir working on this .
+// reduce() method is used to apply function to array elements adding them up into a single
+//value.In the above function is used to concatinate the current element iinto the flattened 
+//array
+
+
+//Qn3.Can you give an example of a nested array that the function can be able to flatten?
+//Yes, here is an example of a nested array that my function above can flatten.
+//let nestedArray =[1,[2,3],[4,,5,6],7]
+// flattenArray(nestedArray)//the function will return flatArray=[1,2,3,4,5,6,7]
+
+//Qn4.Explain how the flat() method can be used to flatten an array in java script.
+// The flat is a build in method in java script used to flatten arrays, It creates a new array
+//with all sub-array elements concatenated  into it recursively up to the specified depth.
+
+// Qn5.What are some potential issues to watch out when flattening an array in javascript?
+//One potential issue to watch out is the risk of creating a very large flattened array, which
+//could lead to performance issues or memory errors.Also the issue of circular references
+//in nested arrays , which could cause infinite recursion if not handled properly
+
+
+
+//Qn1.What is the difference between a map() and foreach()
+//ans: the map() and foreach() are bothe array methods that differ in what  they return.
+// --The map() returns a new array with same length as the original array.
+// --The Foreach() does not return anything but simply it executes a callback function on
+//each element of the array.
+//Example: const arr=[123]
+//map() const newarr=arr.map("array" => array*2)// newarr=[2,4,6]
+//foreach()  const newarr.foreach(num => console.log(num*2))//2,4,6
+
+
+//Qn2. How do you remove an element from an array in javascript?
+//ans: To remove an element from an array you need to use the splice()method.
+//The method modifies the original array by either replacing or removing an element
+//of the array.
+
+//Qn3. What is the difference between a .filter() and .find() method?
+//ans:Both filter() and find() methods are used to search for elements in an array that
+//meet certain standards.
+//filter() returns all the elements that passes a certain  test provided by a callback function
+//find() returns the first element that passes a test provided by a callback function
+let numbers=[2,4,6,7]
+const evenNums=numbers.filter((num) => num%2===0)
+console.log(evenNums)
+
+const firstEvenNum=numbers.find((num) => num%2 ===0)
+console.log(firstEvenNum)
+
+//Qn4.How do you sort an array in javascript?
+//ans: To sort an array u can use the sort() method which modifies the original array by 
+//sorting its elements in place.
+//example:
+const fruits= ['bananas','apple','pineapple','mango']
+fruits.sort()
+console.groupCollapsed(fruits)
+
+
+//Qn5.How do you flatten a nested array in javascript?
+//ans:To flatten an array in javascript you can use the reduce() and concate() methods.
+const integers=[1,2,[3,5],7]
+
+const flattenIntegers=integers.reduce((accumulator,value) => accumulator.concat(value),[])
+console.log(flattenIntegers)
+
+
