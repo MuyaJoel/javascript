0134d2c7452fd27f0ec9ae59df156ebfbc1db22c
Joel <joelmuya@outlook.com>
2024-05-29 13:49:40 +0300

    new

--- Github_Api/app.js
+++ Github_Api/app.js
@@ -3,7 +3,7 @@
 
 
 const fetchData= async ()=>{
-    const token='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'
+    
     const data=document.getElementById('data').value
 
     const url = await fetch(`https://api.github.com/users/${data}`,{headers: {Authorization: `Bearer ${token}`}})
@@ -33,52 +33,47 @@ const getData= ()=>{
 
         const div2=document.createElement('div')
         div2.style.display="flex"
-        div2.style.flexDirection="row"
+        div2.style.flexDirection="column"
         div2.style.justifyContent="center"
         div2.style.alignItems="center"
-        div2.style.columnGap="10px"
+        div2.style.rowGap="10px"
         div.appendChild(div2)
 
         const div3=document.createElement('div')
         div3.style.display="flex"
-        div3.style.flexDirection="row"
+        div3.style.flexDirection="column"
         div3.style.justifyContent="center"
         div3.style.alignItems="center"
-        div3.style.columnGap="10px"
+        div3.style.rowGap="10px"
         div.appendChild(div3)
 
         const image=document.createElement('img')
         image.src=data.avatar_url
-        image.style.width="20%"
-        image.style.height="30%"
-        image.style.borderRadius="50px"
+        image.style.width="18%"
+        image.style.height="20%"
+        image.style.borderRadius="70px"
         div1.appendChild(image)
 
         const name=document.createElement('p')
-        name.textContent=data.login
+        name.textContent=data.name
         name.style.marginLeft="40px"
         name.style.fontSize="20px"
         div1.appendChild(name)
-
+         
         const loc=document.createElement('p')
         loc.textContent=data.location
-        div1.appendChild(loc)
-
-        const follow=document.createElement('p')
-        follow.textContent="Followers"
-        div2.appendChild(follow)
+        div2.appendChild(loc)
 
         const followers=document.createElement('p')
-        followers.textContent=data.followers
+        followers.textContent=`${data.followers} Followers ${data.public_repos} repos`
         div2.appendChild(followers)
 
-        const repo=document.createElement('p')
-        repo.textContent="Repositories"
-        div3.appendChild(repo)
-
-        const repos=document.createElement('p')
-        repos.textContent=data.public_repos
-        div3.appendChild(repos)
+        const uni=document.createElement('p')
+        uni.textContent=`${data.company}`
+        div3.appendChild(uni)
+        const university=document.createElement('p')
+        university.textContent=`Bio:${data.bio}`
+        div3.appendChild(university)
 
        })
        .catch((error)=>{
