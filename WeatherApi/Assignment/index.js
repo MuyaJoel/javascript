02f940aea51be3f237dcb2ffe56de06699dcc71a
Joel <joelmuya@outlook.com>
2024-06-03 15:16:04 +0300

    new

--- WeatherApi/Assignment/index.js
+++ WeatherApi/Assignment/index.js
@@ -1,12 +1,13 @@
 
 const weatherData= async()=>{
+    const search=document.querySelector('#city').value
     const token="c9ff33f324d82af76003491105ccc44d"
-    const url =await fetch(`https://openweathermap.org/api`, {header: {Authorization: `Bearer ${token}`}})
+    const url =await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=c9ff33f324d82af76003491105ccc44d`)
     try{
         if(!url.ok){
             throw new Error('Network Error')
         }
-        return url
+        return url.json()
 
     }catch(error){
         throw error
@@ -17,6 +18,23 @@ const displayData=()=>{
     weatherData()
     .then(data=>{
         console.log(data)
+        const time=document.querySelector('#timezone')
+        time.textContent=`timezone ${data.timezone}`
+        const county=document.querySelector('#county')
+        county.textContent=`${data.name},${data.sys['country']}`
+        const temp=document.querySelector('#tempData')
+        temp.textContent=`Temp ${data.main['temp']}, Humidity ${data.main['humidity']}`
+
+        
+        data.weather.map((rainInfo)=>{
+            const rain=document.querySelector('#rainData')
+            rain.textContent=`${rainInfo.main}, description ${rainInfo.description}`
+            const image=document.querySelector('#icon')
+            image.src=`${rainInfo.icon}`
+        })
+
+        const wind=document.querySelector('#windData')
+        wind.textContent=`Wind speed is ${data.wind['speed']},deg ${data.wind['deg']} & gust ${data.wind['gust']}`
        
     })
     .catch((error)=>{
